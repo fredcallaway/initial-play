8e9994cd4ff4fdb3a2014192feb1757bad26d81f
Gustav Karreskog <gustav.karreskog@gmail.com>
2018-11-10 19:48:55 -0500

    Added SimHeuristics where oppentens play is simulate

--- Heuristics.jl
+++ Heuristics.jl
@@ -1,8 +1,11 @@
 import Distributions: MvNormal
 import Distributions
+import StatsFuns: softmax
+import StatsBase: sample, WeightVec
 import Base
 import DataStructures: OrderedDict
 using BlackBoxOptim
+import Printf: @printf
 
 function sample_cell(ρ; max=10, min=0, µ=5, σ=5)
     if ρ < 1 && ρ > -1
@@ -56,13 +59,28 @@ mutable struct Heuristic
     α_r::Float64
     β_c::Float64
     α_c::Float64
+    λ::Float64
     cell_reduce::Function
-    # α_s::Float64
     s_reduce::Function
 end
 
-Heuristic(dists::OrderedDict) = Heuristic(map(rand, dists.vals)...)
+mutable struct SimHeuristic
+    opp_h::Heuristic
+    self_h::Heuristic
+    λ::Float64
+end
 
+
+Heuristic(dists::OrderedDict) = Heuristic(map(rand, dists.vals)...)
+SimHeuristic(dists::OrderedDict) = SimHeuristic(Heuristic(dists), Heuristic(dists), rand(dists["λ"]))
+
+function print_h(io::IO, h::Heuristic)
+    # println("reduce_funs: ", h.cell_reduce, ", ", h.s_reduce)
+    @printf(io, "Heuristic: funs(%s, %s) vars(β_r=%.2f, α_r=%.2f, β_c=%.2f, α_c=%.2f)", h.cell_reduce, h.s_reduce, h.β_r, h.α_r, h.β_c, h.α_c)
+    # println("β_r=", round(h.β_r,3), " α_r=", h.α_r, " β_c=", h.β_c, " α_c=", h.α_c)
+end
+Base.show(io::IO, h::Heuristic) = @printf(io, "Heuristic: funs(%s, %s) vars(β_r=%.2f, α_r=%.2f, β_c=%.2f, α_c=%.2f)", h.cell_reduce, h.s_reduce, h.β_r, h.α_r, h.β_c, h.α_c)
+Base.show(io::IO, s::SimHeuristic) = begin println("Opp_h: ",s.opp_h); println("Self_h: ",s.self_h); println("λ: ", s.λ) end
 first_el = (x, y) -> x
 
 h_dists = OrderedDict(
@@ -70,12 +88,12 @@ h_dists = OrderedDict(
 "α_r" => Distributions.Uniform(0,4),
 "β_c" => Distributions.Uniform(0,4),
 "α_c" => Distributions.Uniform(0,4),
-"cell_reduce" => [Base.:+, Base.:*, Base.:/, first_el],
-# "α_s" => Distributions.Uniform(0,4),
+"λ" => Distributions.Uniform(0,4),
+"cell_reduce" => [Base.:+, Base.:*, first_el],
 "s_reduce" => [sum, maximum, minimum]
 )
 
-function decide(h::Heuristic, game::Game)
+function relative_values(h::Heuristic, game::Game)
     v = zeros(size(game))
     for i in 1:size(game)
         r = game.row[i, :]
@@ -83,37 +101,40 @@ function decide(h::Heuristic, game::Game)
         r_tilde = map( x-> h.β_r * x^h.α_r, r)
         c_tilde = map( x-> h.β_c * x^h.α_c, c)
         s = map(h.cell_reduce, r_tilde, c_tilde)
-        # s_tilde = map( x-> x^h.α_s, s) TODO: Remeber that you did this!!
-        s_tilde = map( x-> x, s)
-        v[i] = h.s_reduce(s_tilde)
+        v[i] = h.s_reduce(s)
     end
+    return v
+end
+
+function decide(h::Heuristic, game::Game)
+    v = relative_values(h, game)
+    v = softmax(h.λ*v)
     choice = argmax(v)
 end
 
-function mutate_heuristic(h, p, h_dists)
-    for name in fieldnames(Heuristic)
-        if rand() < p
-            println("mutating")
-            setfield!(h, name, rand(h_dists["$name"]))
-        end
-    end
+function decide(s::SimHeuristic, game::Game)
+    g = deepcopy(game)
+    opp_v = relative_values(s.opp_h, transpose(g))
+    weights = softmax(s.λ*opp_v)
+    g.row .*= weights'
+    choice = decide(s.self_h, g)
+    return choice
 end
-
-
 
 
 
 ### Generating games and calcuating fitness
-level_0 = Heuristic(0,0,0,0, (+), sum)
-level_1 = Heuristic(1,1,0,0, (+), sum)
-maximin = Heuristic(1,1,0,0, (+), minimum)
-sum_prod = Heuristic(1,1,1,1, (*), sum)
-max_prod = Heuristic(1,1,1,1, (*), maximum)
-max_sum = Heuristic(1,1,1,1, (+), maximum)
-sum_sum = Heuristic(1,1,1,1, (+), sum)
+level_0 = Heuristic(0,0,0,0,0, (+), sum)
+level_1 = Heuristic(1,1,0,0,0, (+), sum)
+level_2 = SimHeuristic(level_1, level_1, 4.)
+maximin = Heuristic(1,1,0,0,0, (+), minimum)
+sum_prod = Heuristic(1,1,1,1,0, (*), sum)
+max_prod = Heuristic(1,1,1,1,0, (*), maximum)
+max_sum = Heuristic(1,1,1,1,0, (+), maximum)
+sum_sum = Heuristic(1,1,1,1,0, (+), sum)
 
 
-function fitness(h, opp_h, games)
+function payoff(h, opp_h, games)
     payoff = 0
     for g in games
         decision = decide(h, g)
@@ -128,44 +149,74 @@ opp_cols_played = (opp_h, games) -> [g.row[:,decide(opp_h, transpose(g))] for g 
 function fitness_from_col_play(h, games, opp_plays)
     payoff = 0
     for i in 1:length(games)
-        decision = decide(h,games[i])
+        decision = decide(h, games[i])
         payoff += opp_plays[i][decision]
     end
     return payoff
 end
 
 
-function opt_h!(h, opp_h, games)
-    opp_plays = opp_cols_played(opp_h, training_games)
+function opt_h!(h::Heuristic, opp_h::Heuristic, games; max_time=10.0)
+    opp_plays = opp_cols_played(opp_h, games)
     param_names = [:β_r, :α_r, :β_c, :α_c]
     init_params = [h.β_r, h.α_r, h.β_c, h.α_c]
     function opt_fun(params)
         for (name, param) in zip(param_names, params)
             setfield!(h, name, param)
         end
-        return -fitness_from_col_play(h, training_games, opp_plays)
+        return -fitness_from_col_play(h, games, opp_plays)
     end
     # res = bboptimize(opt_fun; SearchRange = (h_dists["β_r"].a ,h_dists["β_r"].b), NumDimensions = 4, MaxTime=10.0, Method = :separable_nes, TraceMode = :silent, TraceInterval =5.0)
-    res = bboptimize(opt_fun; SearchRange = (h_dists["β_r"].a ,h_dists["β_r"].b), NumDimensions = 4, MaxTime=10.0, TraceMode = :silent, TraceInterval =5.0)
+    res = bboptimize(opt_fun; SearchRange = (h_dists["β_r"].a ,h_dists["β_r"].b), NumDimensions = 4, MaxTime=max_time, TraceMode = :silent, TraceInterval =5.0)
     for (name, param) in zip(param_names, best_candidate(res))
         setfield!(h, name, param)
     end
 end
 
+function opt_s!(s::SimHeuristic, opp_h::Heuristic, games; max_time=10.0)
+    opp_plays = opp_cols_played(opp_h, games)
+    param_names = [(s.opp_h,:β_r), (s.opp_h,:α_r), (s.opp_h,:β_c), (s.opp_h,:α_c), (s.self_h,:β_r), (s.self_h, :α_r), (s.self_h, :β_c), (s.self_h, :α_c), (s,:λ)]
+    init_params = [s.opp_h.β_r, s.opp_h.α_r, s.opp_h.β_c, s.opp_h.α_c, s.self_h.β_r, s.self_h.α_r, s.self_h.β_c, s.self_h.α_c, s.λ]
+    function opt_fun(params)
+        for (name, param) in zip(param_names, params)
+            setfield!(name[1], name[2], param)
+        end
+        return -fitness_from_col_play(s, games, opp_plays)
+    end
+    # res = bboptimize(opt_fun; SearchRange = (h_dists["β_r"].a ,h_dists["β_r"].b), NumDimensions = 9, MaxTime=10.0, Method = :separable_nes, TraceMode = :silent, TraceInterval =5.0)
+    res = bboptimize(opt_fun; SearchRange = (h_dists["β_r"].a ,h_dists["β_r"].b), NumDimensions = 9, MaxTime=max_time, TraceMode = :silent, TraceInterval =5.0)
+    for (name, param) in zip(param_names, best_candidate(res))
+        setfield!(name[1], name[2], param)
+    end
+end
+
+
 function find_best_heuristic(h_dists, opp_h, games)
     h_list = []
-    for cell_reduce in h_dists["cell_reduce"], s_reduce in h_dists["s_reduce"]
-        h = Heuristic(h_dists)
-        h.cell_reduce = cell_reduce
-        h.s_reduce = s_reduce
-        println("pre-fitnes: ", fitness(h, opp_h, games), " for ", h)
-        opt_h!(h, opp_h, training_games)
-        new_fitness = fitness(h, opp_h, games)
-        println("post-fitnes: ", new_fitness, " for ", h)
-        append!(h_list, [(h, new_fitness)])
+    # for cell_reduce in h_dists["cell_reduce"], s_reduce in h_dists["s_reduce"]
+    #     h = Heuristic(h_dists)
+    #     h.cell_reduce = cell_reduce
+    #     h.s_reduce = s_reduce
+    #     println("pre-fitnes: ", payoff(h, opp_h, games), " for ", h)
+    #     opt_h!(h, opp_h, games)
+    #     new_fitness = payoff(h, opp_h, games)
+    #     println("post-fitnes: ", new_fitness, " for ", h)
+    #     append!(h_list, [(h, new_fitness)])
+    # end
+    for opp_cell_reduce in h_dists["cell_reduce"], opp_s_reduce in h_dists["s_reduce"], self_cell_reduce in h_dists["cell_reduce"], self_s_reduce in h_dists["s_reduce"]
+        s = SimHeuristic(h_dists)
+        s.opp_h.cell_reduce = opp_cell_reduce
+        s.opp_h.s_reduce = opp_s_reduce
+        s.self_h.cell_reduce = self_cell_reduce
+        s.self_h.s_reduce = self_s_reduce
+        println("pre-fitnes: ", payoff(s, opp_h, games), " for ", s)
+        opt_s!(s, opp_h, games)
+        new_fitness = payoff(s, opp_h, games)
+        println("post-fitnes: ", new_fitness, " for ", s)
+        append!(h_list, [(s, new_fitness)])
     end
-    sort!(h_list, rev=true, by= x -> x[2])
-    return(h_list)
+    sort!(s_list, rev=true, by= x -> x[2])
+    return(s_list)
 end
 
 
@@ -178,23 +229,24 @@ println("Compare with ρ = ", ρ)
 training_games = [Game(2, ρ) for i in range(1,n_games)];
 test_games = [Game(2, ρ) for i in range(1,n_games)];
 
-println("Level-0: \t", fitness(level_0, opp_h, training_games))
-println("Level-1: \t", fitness(level_1, opp_h, training_games))
-println("Maximin: \t", fitness(maximin, opp_h, training_games))
-println("Sum prod: \t",fitness(sum_prod, opp_h, training_games))
-println("Max prod: \t", fitness(max_prod, opp_h, training_games))
-println("Max sum: \t", fitness(max_sum, opp_h, training_games))
-println("Sum sum: \t", fitness(sum_sum, opp_h, training_games))
+println("Level-0: \t", payoff(level_0, opp_h, training_games))
+println("Level-1: \t", payoff(level_1, opp_h, training_games))
+println("Level-2: \t", payoff(level_2, opp_h, training_games))
+println("Maximin: \t", payoff(maximin, opp_h, training_games))
+println("Sum prod: \t",payoff(sum_prod, opp_h, training_games))
+println("Max prod: \t", payoff(max_prod, opp_h, training_games))
+println("Max sum: \t", payoff(max_sum, opp_h, training_games))
+println("Sum sum: \t", payoff(sum_sum, opp_h, training_games))
 
 
-# println("0 0  ", fitness(level_0, level_0, training_games))
-# println("0 1  ", fitness(level_0, level_1, training_games))
-# println("1 0  ", fitness(level_1, level_0, training_games))
-# println("1 1  ", fitness(level_1, level_1, training_games))
+# println("0 0  ", payoff(level_0, level_0, training_games))
+# println("0 1  ", payoff(level_0, level_1, training_games))
+# println("1 0  ", payoff(level_1, level_0, training_games))
+# println("1 1  ", payoff(level_1, level_1, training_games))
 
 ### Compare Heuristics
 # heuristics = [Heuristic(h_dists) for i in 1:200]
-# perf = map(h -> fitness(h, opp_h, training_games), heuristics)
+# perf = map(h -> payoff(h, opp_h, training_games), heuristics)
 # best_h = heuristics[argmax(perf)]
 # println("Best (",maximum(perf), "):", heuristics[argmax(perf)])
 
@@ -207,14 +259,15 @@ println("Best (",best_h_fit, "):", best_h)
 
 println("Test games:")
 
-println("Best h: \t", fitness(best_h, opp_h, test_games))
-println("Level-0: \t", fitness(level_0, opp_h, test_games))
-println("Level-1: \t", fitness(level_1, opp_h, test_games))
-println("Maximin: \t", fitness(maximin, opp_h, test_games))
-println("Sum prod: \t",fitness(sum_prod, opp_h, test_games))
-println("Max prod: \t", fitness(max_prod, opp_h, test_games))
-println("Max sum: \t", fitness(max_sum, opp_h, test_games))
-println("Sum sum: \t", fitness(sum_sum, opp_h, test_games))
+println("Best h: \t", payoff(best_h, opp_h, test_games))
+println("Level-0: \t", payoff(level_0, opp_h, test_games))
+println("Level-1: \t", payoff(level_1, opp_h, test_games))
+println("Level-2: \t", payoff(level_2, opp_h, test_games))
+println("Maximin: \t", payoff(maximin, opp_h, test_games))
+println("Sum prod: \t",payoff(sum_prod, opp_h, test_games))
+println("Max prod: \t", payoff(max_prod, opp_h, test_games))
+println("Max sum: \t", payoff(max_sum, opp_h, test_games))
+println("Sum sum: \t", payoff(sum_sum, opp_h, test_games))
 
 
 prisoners_dilemma = Game([[3 0];[4 1]], [[3 4]; [0 1]])
@@ -223,9 +276,9 @@ print(decide(best_h, prisoners_dilemma))
 
 
 h = Heuristic(h_dists)
-println("pre-fitnes: ", fitness(h, opp_h, training_games), " for ", h)
+println("pre-fitnes: ", payoff(h, opp_h, training_games), " for ", h)
 opt_h!(h, opp_h, training_games)
-new_fitness = fitness(h, opp_h, training_games)
+new_fitness = payoff(h, opp_h, training_games)
 println("post-fitnes: ", new_fitness, " for ", h)
 println("Prisoners dilemma test")
 print(decide(best_h, prisoners_dilemma))
