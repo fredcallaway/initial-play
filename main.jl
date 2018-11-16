82155a28044c172db2c8e25f7e3f601eac043b7b
fredcallaway <fredc@llaway.com>
2018-11-15 19:22:59 -0500

    rename train_games

--- main.jl
+++ main.jl
@@ -12,7 +12,7 @@ end
     ρ = 0.
     game_size = 3
     n_games = 1000
-    level = 3
+    level = 2
     # n_inits = 8
     n_inits = Sys.CPU_THREADS
     bounds = Bounds([0., -1., 0.], [1., 1., 10.])
@@ -20,23 +20,26 @@ end
     opp_h = Heuristic(0, 0, 10)
 end
 
-games = [Game(game_size, ρ) for i in range(1,n_games)]
-opp_plays = opp_cols_played(opp_h, games)
+train_games = [Game(game_size, ρ) for i in range(1,n_games)]
+train_opp_plays = opp_cols_played(opp_h, train_games)
 test_games = [Game(game_size, ρ) for i in range(1,n_games)]
 test_opp_plays = opp_cols_played(opp_h, test_games);
 
+opt_level_2 = SimHeuristic([opp_h, Heuristic(0.,0.,4.)])
+opt_level_2_cheap = SimHeuristic([Heuristic(0.,0.,2.), Heuristic(0.,0.,3.)])
+
 @everywhere begin
-    games = $games
+    train_games = $train_games
     test_games = $test_games
-    opp_plays = $opp_plays
+    train_opp_plays = $train_opp_plays
     test_opp_plays = $test_opp_plays
 
 end
 
 x_inits = [rand(bounds, level) for i in 1:n_inits]
 @time res = pmap(x_inits) do x
-    h = optimize_h(level, games, opp_plays, costs; init_x=x)
-    train_score = -loss(h, games, opp_plays, costs)
+    h = optimize_h(level, train_games, train_opp_plays, costs; init_x=x)
+    train_score = -loss(h, train_games, train_opp_plays, costs)
     test_score = -loss(h, test_games, test_opp_plays, costs)
     # println("Train score: ", train_score, "   Test score:  ", test_score)
     (h, train_score, test_score)
@@ -52,12 +55,14 @@ for (h, trn, tst) in res
 end
 
 sort!(res, by= x -> x[2], rev=true)
-
 println("Best h on training", res[1])
 sort!(res, by= x -> x[3], rev=true)
 println("Best h on test", res[1])
+@printf("Level-2 exact perf: %.0f", loss(level_2, train_games, train_opp_plays, costs))
+@printf("Level-2 cheap perf: %.0f", loss(level_2_cheap, train_games, train_opp_plays, costs))
 
-optimize_h(level, games, opp_plays, costs)
+
+optimize_h(level, train_games, opp_plays, costs)
 
 
 #
