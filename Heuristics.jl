d645ccbcda499e9f43181ba7b959a3623bca8b2f
fredcallaway <fredc@llaway.com>
2018-11-21 13:50:46 -0800

    Add Game to Heuristics.jl and refactor RowHeuristic

--- Heuristics.jl
+++ Heuristics.jl
@@ -1,12 +1,78 @@
+import Distributions: MvNormal
+import Distributions
+import StatsBase: sample, Weights
+import Statistics: mean
+import Base
+import DataStructures: OrderedDict
+using Optim
+using JSON
+import Printf: @printf, @sprintf
+# import Base: rand
 
-#%% Temporary
-struct Game
-    row::Matrix{Float64}
-    col::Matrix{Float64}
+function softmax(x)
+    ex = exp.(x)
+    ex ./= sum(ex)
+    ex
 end
-Game() = Game(rand(2,2), rand(2,2))
-#%%
 
+#%% ==================== Games ==================== %%#
+
+function sample_cell(ρ; max=9.5, min=-0.5, µ=5, σ=5)
+    if ρ < 1 && ρ > -1
+        Σ = [[1 ρ]
+             [ρ 1]]
+        dist = MvNormal(Σ)
+        r, c = rand(dist)*σ .+ μ
+        while any([r, c] .< min) || any([r, c] .> max)
+             r, c = rand(dist)*σ .+ μ
+        end
+    else
+        r = randn()*σ + μ
+        c = ρ*(r - μ) + μ
+        while any([r, c] .< min) || any([r, c] .> max)
+            r = randn()*σ + μ
+            c = ρ*(r - μ) + μ
+        end
+    end
+    r = round(r)
+    c = round(c)
+    return (r, c)
+end
+
+struct Game
+    row::Matrix{Real}
+    col::Matrix{Real}
+end
+
+Game(size::Int, ρ::Number) = begin
+    X = [sample_cell(ρ) for i in 1:size, j in 1:size]
+    Game(
+        map(x->x[1], X),
+        map(x->x[2], X),
+    )
+end
+
+Base.transpose(g::Game) = Game(transpose(g.col), transpose(g.row))
+Base.size(g::Game) = size(g.row)[1]
+
+function normalize(g::Game)
+    Game(g.row .- mean(g.row), g.col .- mean(g.col))
+end
+
+Base.show(io::IO, g::Game) = begin
+    for i in 1:size(g)
+        for j in 1:size(g)
+            # print(Int(g.row[i,j]), ",", Int(g.col[i,j]), "  ")
+            @printf("%+3.1f , %+3.1f | ", g.row[i,j], g.col[i,j])
+        end
+        println()
+    end
+end
+
+Game(3, 0.)
+
+
+#%% ==================== Abstract Heuristic ====================
 abstract type Heuristic end
 
 function play_distribution(h::Heuristic, g::Game)
@@ -17,10 +83,11 @@ function cost(h::Heuristic)
     error("Unimplemented")
 end
 
-function size(h::Heuristic)
+function Base.size(h::Heuristic)
     # Returns the number of real valued parameters
     error("Unimplemented")
 end
+
 function rand_params(h::Heuristic)
     error("Unimplemented")
 end
@@ -36,20 +103,44 @@ function expected_payoff(h::Heuristic, opponent::Heuristic, g::Game)
     sum(p_outcome .* g.row)
 end
 
+
+#%% ==================== RowHeuristic ====================
+
 mutable struct RowHeuristic <: Heuristic
     α::Real  # we might get a performance boost by using a parametric typem
     γ::Real
     λ::Real
 end
 
+function row_values(h::RowHeuristic, g::Game)
+    map(1:size(g)) do i
+        r = @view g.row[i, :]
+        c = @view g.col[i, :]
+        # a = h.α / max(1., (maximum(c) - minimum(c)))  # NOTE: Do we want this?
+        s = @. r / (1 + exp(-h.α * c))
+        k = h.γ / max(1., (maximum(s) - minimum(s)))
+        v = s' * softmax(k * s)
+    end
+end
+
+function play_distribution(h::RowHeuristic, g::Game)
+    softmax(h.λ * row_values(h, g))
+end
+
+#%% ==================== SimHeuristic ====================
 mutable struct SimHeuristic <: Heuristic
     h_list::Vector{Heuristic}
     level::Int64
 end
 
+
+#%% ==================== CacheHeuristic ====================
 struct CacheHeuristic <: Heuristic
     cache::Dict{Game, Vector{Float64}}
 end
 
-
-RowHeuristic
+struct MetaHeuristic <: Heuristic
+end
+# TODO: prior distribution on Heuristic weights
+# - cost of deviating from prior for a specific game
+# - optimize prior for an environment (list of games)
