02515e52bffbf6df834690fc4dd3945efec6949d
Gustav Karreskog <gustav.karreskog@gmail.com>
2019-04-01 12:05:56 -0400

    Some extra comments

--- DeepLayers.jl
+++ DeepLayers.jl
@@ -9,17 +9,18 @@ function my_softmax(x)
     ex
 end
 
-
+# Game_Dense_Full incorporates information about the games directly, including max,min, and mean of rows and columns.
 mutable struct Game_Dense_full
   Wᴿ
-  WPᴿ
+  WPᴿ # Paramaters for incorporating game information for row player
   Wᶜ
-  WPᶜ
+  WPᶜ # Paramaters for incorporating game information for column player
   bᴿ
   bᶜ
   σ
 end
 
+# Only looks at previous layer
 mutable struct Game_Dense
   Wᴿ
   Wᶜ
@@ -28,15 +29,17 @@ mutable struct Game_Dense
   σ
 end
 
+# Update functions if game information is directly incorporated
 tensor_mul(H, W, WP, US, b, σ) = begin
   [σ.(W * H[i,j] .+ sum([w *u[i,j] for (w,u) in zip(WP,US)]) .+ b) for i in 1:size(H)[1], j in 1:size(H)[2]]
 end
 
+# Update function with only previous layer
 tensor_mul(H, W, b, σ) = begin
   [σ.(W * H[i,j] .+ b) for i in 1:size(H)[1], j in 1:size(H)[2]]
 end
 
-
+# Handling some weird error where sometimes the output is sent as a tuple
 (m::Game_Dense)(t) = m(t...)
 (m::Game_Dense_full)(t) = m(t...)
 
@@ -48,6 +51,8 @@ end
   tensor_mul(Hᴿ, m.Wᴿ, m.bᴿ, m.σ), tensor_mul(Hᶜ, m.Wᶜ, m.bᶜ,m.σ), US, Uᴿ, Uᶜ
 end
 
+# Allows us to take games directly as input. Also generates a vector with different transformations of payoff matrices
+# such as max of rows/columns.
 (m::Game_Dense)(g::Game) = m(g.row, g.col, [g.row, g.col, [mapslices(x -> f(x)*ones(length(x)), u, dims=d) for f in [maximum, mean, minimum], d in [1,2], u in [g.row, g.col]]...], g.row, g.col)
 (m::Game_Dense_full)(g::Game) = m(g.row, g.col, [g.row, g.col, [mapslices(x -> f(x)*ones(length(x)), u, dims=d) for f in [maximum, mean, minimum], d in [1,2], u in [g.row, g.col]]...], g.row, g.col)
 
@@ -69,38 +74,29 @@ Game_Dense_full(in::Integer, out::Integer, σ = identity) = begin
   Game_Dense_full(Wᴿ, WPᴿ, Wᶜ, WPᶜ, bᴿ, bᶜ, σ)
 end
 
+# Turns a previous layer of matrices into a a single prediction based on a weighted softmax of each previous layer
 mutable struct Game_Soft
   Wᴿ
   Wᶜ
 end
-# (m::Game_Soft)(Hᴿ, Hᶜ, US, Uᴿ, Uᶜ) = m(Hᴿ, Hᶜ, Uᴿ, Uᶜ)
+
 (m::Game_Soft)(t) = m(t...)
 (m::Game_Soft)(Hᴿ, Hᶜ, US, Uᴿ, Uᶜ) = begin
   n_rows = size(Hᴿ)[1]
   n_cols = size(Hᴿ)[2]
-  # n_rows = size(Hᴿ[1])[1]
-  # n_cols = size(Hᴿ[1])[2]
-  # wᴿ = max.(0,m.Wᴿ)
-  # wᴿ = wᴿ/sum(wᴿ)
-  wᴿ = softmax(m.Wᴿ)
+  wᴿ = softmax(m.Wᴿ) # To force it into the simplex
   fᴿ = [my_softmax(sum([Hᴿ[i,j][k] for i in 1:n_rows, j in 1:n_cols], dims=2)) for k in 1:length(Hᴿ[1,1])]
-  # fᴿ = [my_softmax(sum(Hᴿ[:,:,k], dims=2)) for k in 1:size(Hᴿ)[3]]
-  # fᴿ = [my_softmax(sum(h, dims=2)) for h in Hᴿ]
-  # fᴿ = mapslices(x -> my_softmax(sum(x, dims=2)), Hᴿ, dims=(1,2))
   aᴿ = [sum([fᴿ[k][i]*wᴿ[k] for k in 1:length(fᴿ)]) for i in 1:n_rows]
-  # wᶜ = max.(0,m.Wᶜ)
-  # wᶜ = wᶜ/sum(wᶜ)
+
   wᶜ = softmax(m.Wᶜ)
   fᶜ = [my_softmax(transpose(sum([Hᶜ[i,j][k] for i in 1:n_rows, j in 1:n_cols], dims=1))) for k in 1:length(Hᴿ[1,1])]
-  # fᶜ = [my_softmax(transpose(sum(Hᶜ[:,:,k], dims=1))) for k in 1:size(Hᶜ)[3]]
-  # fᶜ = [my_softmax(transpose(sum(h, dims=1))) for h in Hᶜ]
   aᶜ = [sum([fᶜ[k][i]*wᶜ[k] for k in 1:length(fᶜ)]) for i in 1:n_cols]
   return [aᴿ], [aᶜ], Uᴿ, Uᶜ
 end
 
 Game_Soft(in::Integer) = Game_Soft(param(randn(in)), param(randn(in)))
 
-
+# Action response layers weights the prediction of previous levels and logit-best responds
 mutable struct Action_Response
   vᴿ
   vᶜ
@@ -114,11 +110,7 @@ Action_Response(level::Integer) = Action_Response(randn(level), randn(level), 3*
   n_rows = size(Uᴿ)[1]
   n_cols = size(Uᴿ)[2]
   level = length(Aᴿ)
-  # vᴿ = max.(0., m.vᴿ)
-  # vᴿ = vᴿ/sum(vᴿ)
   vᴿ = my_softmax(m.vᴿ)
-  # vᶜ = max.(0., m.vᶜ)
-  # vᶜ = vᶜ/sum(vᶜ)
   vᶜ = my_softmax(m.vᶜ)
   λᴿ = max(0., m.λᴿ)
   λᶜ = max(0., m.λᶜ)
@@ -127,6 +119,7 @@ Action_Response(level::Integer) = Action_Response(randn(level), randn(level), 3*
   return [Aᴿ..., aᴿ], [Aᶜ..., aᶜ], Uᴿ, Uᶜ
 end
 
+# The last layer weights all actions response layers, including the "0" layer from Game_Soft
 mutable struct Last
   v
 end
@@ -137,13 +130,12 @@ Last(level::Integer) = Last(randn(level))
 
 (m::Last)(Aᴿ, Aᶜ, Uᴿ, Uᶜ) =  begin
   level = length(Aᴿ)
-  # v = max.(0., m.v)
-  # v = v/sum(v)
   v = my_softmax(m.v)
   return sum(Aᴿ[l] * v[l] for l in 1:level)
 end
 
 
+# Makes the accesible for flux functions such as params, or Chain
 Flux.@treelike Game_Dense
 Flux.@treelike Game_Dense_full
 Flux.@treelike Game_Soft
