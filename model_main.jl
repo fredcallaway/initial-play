e3491dff709d022118cf73e04353092d79fe14e6
fredcallaway <fredc@llaway.com>
2020-01-16 15:25:37 -0800

    fix parallel and abstract out results_df

--- model_main.jl
+++ model_main.jl
@@ -70,9 +70,9 @@ function fit_costs(model, train, space; n_sobol=500, n_gp=50)
     f = make_loss(model, train, space; parallel=false)
     xs = init_points(n_free(space), n_sobol)
     @time ys = pmap(f, xs)
+    f = make_loss(model, train, space; parallel=true)
     @time gp_opt = gp_minimize(f, n_free(space), init_Xy=(combinedims(xs), ys), iterations=n_gp)
 
-    f = make_loss(model, train, space; parallel=true)
     costs = get_cost_type(model)(;space(gp_opt.observed_optimizer)...)
     trained_models = pmap(collect(all_data)) do (treat, data)
         treat => train(model, data, train_idx, costs)
@@ -90,23 +90,28 @@ mh_space = Box(
     :m_λ => (0.5,2.5, :log),
 )
 
-results = Dict(
-    :fit => fit_costs(mh_base, fit_model, mh_space)
+mh_results = Dict(
+    :fit => fit_costs(mh_base, fit_model, mh_space),
     :opt => fit_costs(mh_base, optimize_model, mh_space)
 )
 
 # %% save results
-df = mapmany(collect(results)) do (mode, res)
-    mapmany(collect(res[2])) do (train_treat, model)
-        map([:neg, :pos]) do test_treat
-            y = prediction_loss(model, all_data[test_treat], test_idx, costs)
-            # println("$mode $treat $(round(y; digits=3))",)
-            (test=test_treat, mode=mode, train=train_treat, loss=y)
+
+function results_df(results)
+    df = mapmany(collect(results)) do (mode, res)
+        costs, trained_models = res
+        mapmany(trained_models) do (train_treat, model)
+            map([:neg, :pos]) do test_treat
+                y = prediction_loss(model, all_data[test_treat], test_idx, costs)
+                # println("$mode $treat $(round(y; digits=3))",)
+                (test=test_treat, mode=mode, train=train_treat, loss=y)
+            end
         end
-    end
-end |> DataFrame
-sort!(df, cols=(:test, :mode))
-CSV.write("results/gp_mh.csv", df)
+    end |> DataFrame
+    sort!(df, (:test, :mode))
+end
+
+result_df(results) |> CSV.write("results/gp_mh.csv")
 
 
 map(collect(keys(all_data))) do treat
@@ -136,6 +141,8 @@ deep_results = Dict(
     :fit => fit_costs(deep_base, fit_model, deep_space),
     :opt => fit_costs(deep_base, optimize_model, deep_space)
 )
+
+results_df(deep_results) |> CSV.write("results/deep.csv")
 
 # %% ====================  ====================
 
